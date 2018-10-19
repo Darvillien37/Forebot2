8b5e2c4e649542e161f0ff9fde11926459915d25
Darvillien37 <36336567+Darvillien37@users.noreply.github.com>
2018-10-19 17:25:58 +0100

    Added more unit tests

--- xUnitTesting/ConfigFileTests.cs
+++ xUnitTesting/ConfigFileTests.cs
@@ -1,5 +1,6 @@
 ﻿using Forebot2;
 using System;
+using System.IO;
 using Xunit;
 
 namespace xUnitTesting
@@ -10,9 +11,39 @@ namespace xUnitTesting
         [Fact]
         public void PropertiesTests()
         {
-            
-            //string defaultPath = ConfigurationFile.
+            string expectedFileName = "Forebot.cfg";
+            string expectedDefaultDirectory = @".\";
+            string expectedDefaultPath = Path.Combine(expectedDefaultDirectory, expectedFileName);
 
+            //Check that the config file name is correct:
+            string actual = ConfigurationFile.Name;
+            Assert.NotNull(actual);
+            Assert.Equal(expectedFileName, actual);          
+
+            //Check that the default directory is correct:
+            actual = ConfigurationFile.Location;
+            Assert.NotNull(actual);
+            Assert.Equal(expectedDefaultDirectory, actual);
+
+            //Check that the default path is correct:
+            actual = ConfigurationFile.FullPath;
+            Assert.NotNull(actual);
+            Assert.Equal(expectedDefaultPath, actual);
+
+
+            string expectedDirectory = @"F:\";
+            string expectedPath = Path.Combine(expectedDirectory, expectedFileName);
+
+            //Check that changing the directory is correct:
+            ConfigurationFile.Location = expectedDirectory;
+            actual = ConfigurationFile.Location;
+            Assert.NotNull(actual);
+            Assert.Equal(expectedDirectory, actual);
+
+            //Check that after changing the directory, the path is correct:
+            actual = ConfigurationFile.FullPath;
+            Assert.NotNull(actual);
+            Assert.Equal(expectedPath, actual);
         }
     }
 }
--- xUnitTesting/LoggerTests.cs
+++ xUnitTesting/LoggerTests.cs
@@ -1,9 +1,51 @@
 using Forebot2;
 using System;
+using System.IO;
 using Xunit;
 
 namespace xUnitTesting
 {
+    /// <summary>A class used to test the Text from the console output.</summary>
+    class MockConsoleOutput : IDisposable
+    {
+        /// <summary>
+        /// The mock console
+        /// </summary>
+        private StringWriter mockConsole;
+        /// <summary>
+        /// The original output of the console.
+        /// </summary>
+        private readonly TextWriter originalOutput;
+
+        /// <summary>
+        /// Construct a new mock console output, use <see cref="Dispose"/> to return to original output.
+        /// </summary>
+        public MockConsoleOutput()
+        {
+            mockConsole = new StringWriter();
+            originalOutput = Console.Out;
+            Console.SetOut(mockConsole);
+        }      
+
+        /// <summary>
+        /// Get the output of the console.
+        /// </summary>
+        /// <returns>A string representation of the console text.</returns>
+        public string GetOuput()
+        {
+            return mockConsole.ToString();
+        }
+
+        /// <summary>
+        /// Dispose of the mock console, and return it to the original output.
+        /// </summary>
+        public void Dispose()
+        {
+            Console.SetOut(originalOutput);
+            mockConsole.Dispose();
+        }
+    }
+
     public class LoggerTests
     {
         [Fact]
@@ -18,5 +60,32 @@ namespace xUnitTesting
             Assert.Throws<ArgumentNullException>(() =>
                 ApplicationLogger.Log(null, null, LOG_SEVERITY.DEBUG));
         }
+
+        [Theory]
+        [InlineData(LOG_SEVERITY.DEBUG)]
+        [InlineData(LOG_SEVERITY.VERBOSE)]
+        [InlineData(LOG_SEVERITY.INFO)]
+        [InlineData(LOG_SEVERITY.WARNING)]
+        [InlineData(LOG_SEVERITY.ERROR)]
+        [InlineData(LOG_SEVERITY.CRITICAL)]
+        public void LoggerFormattingTest(LOG_SEVERITY sev)
+        {
+            const string message = "This is a test message";
+            const string source = "Testing";
+
+            string expected;
+            string actual;
+            using (MockConsoleOutput mco = new MockConsoleOutput())
+            {
+                ApplicationLogger.Log(message, source, sev);
+                expected = $"{DateTime.Now} [{sev,8}] {source}: {message}\r\n";//Note the '\r\n' is required as it writes a new line to the console.
+
+                actual = mco.GetOuput();
+                Assert.NotNull(actual);
+                Assert.Equal(expected, actual);
+            }
+         
+        }
+
     }
 }
