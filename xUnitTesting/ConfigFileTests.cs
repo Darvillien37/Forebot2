ce0ca816d05031000c8d481cc9e27e3c1f70b25a
Darvillien37 <36336567+Darvillien37@users.noreply.github.com>
2018-10-19 19:09:56 +0100

    Added more unit tests

--- xUnitTesting/ConfigFileTests.cs
+++ xUnitTesting/ConfigFileTests.cs
@@ -8,6 +8,8 @@ namespace xUnitTesting
     public class ConfigFileTests
     {
 
+        private const string TESTING_CONFIG_DIRECTORY = @"..\..\..\ConfigFileTesting";
+
         [Fact]
         public void PropertiesTests()
         {
@@ -18,7 +20,7 @@ namespace xUnitTesting
             //Check that the config file name is correct:
             string actual = ConfigurationFile.Name;
             Assert.NotNull(actual);
-            Assert.Equal(expectedFileName, actual);          
+            Assert.Equal(expectedFileName, actual);
 
             //Check that the default directory is correct:
             actual = ConfigurationFile.Location;
@@ -44,6 +46,41 @@ namespace xUnitTesting
             actual = ConfigurationFile.FullPath;
             Assert.NotNull(actual);
             Assert.Equal(expectedPath, actual);
+        }
+
+        [Fact]
+        public void FileNonExistantTest()
+        {
+            string TestingDirectory = TESTING_CONFIG_DIRECTORY + @"\FileNonExistantTest";
+            ConfigurationFile.Location = TestingDirectory;
+
+            //Confirm first that the file doesn't exist.
+            Assert.False(File.Exists(ConfigurationFile.FullPath));
+
+            //Confirm that the processing the file returns false, as it has only just been created, and it doesn't contain correct data.
+            Assert.False(ConfigurationFile.ProcessConfigFile());
+
+            //Confirm File has now been created.
+            Assert.True(File.Exists(ConfigurationFile.FullPath));
+
+            //Now delete the file as we don't want to use it anymore.
+            File.Delete(ConfigurationFile.FullPath);
+
+            //Confirm that it has been deleted.
+            Assert.False(File.Exists(ConfigurationFile.FullPath));
+
+        }
+
+        [Fact]
+        public void ValidFileTest()
+        {
+            string TestingDirectory = TESTING_CONFIG_DIRECTORY + @"\ValidFileTest";
+            ConfigurationFile.Location = TestingDirectory;
+            //Confirm that an already valid file exists for this test:            
+            Assert.True(File.Exists(ConfigurationFile.FullPath));
+
+            //Confirm that the file has been processed correctly:
+            Assert.True(ConfigurationFile.ProcessConfigFile(), "Processing was not correct");
         }
     }
 }
